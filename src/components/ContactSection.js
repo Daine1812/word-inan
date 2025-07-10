f3650daf2ce9c30dcee4acec3b9b8648566426ed
Daine1812 <150333512+Daine1812@users.noreply.github.com>
2025-07-11 02:55:05 +0700

    Update ContactSection.js

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -23,7 +23,7 @@ const ContactSection = () => {
       );
 
       // 2. Gửi dữ liệu về backend
-      const res = await fetch('http://localhost:5000/api/contact', {
+      const res = await fetch('https://inan.onrender.com/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, message }),
